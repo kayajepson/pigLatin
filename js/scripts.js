9871c57feb1be95264006a31c272fa2885e03592
Kaya Jepson <jepsonkaya@gmail.com>
2019-04-04 03:29:37 +0000

    idk bryan

--- js/scripts.js
+++ js/scripts.js
@@ -2,61 +2,40 @@
 var vowels = ["a", "e", "i", "o", "u"];
 var exceptions = ["qu"];
 var translate = function(sentence) {
-  // if (sentence.includes("h")) {
-  //   console.log("true");
-  // // } else {
-  // //   return sentence.val();
-  //   }
+
 };
 
-
-
-
+function findVowelIndex(characters) {
+  for (var i = 0; i <= characters.length - 1; i++) {
+    if (vowels.includes(characters[i])) {
+      return i;
+    }
+  }
+  console.log("didn't find a vowel");
+  return -1;
+}
 
 
 // jQuery user interface logic here.
 $(document).ready(function() {
   $("form#pigLatin").submit(function(event) {
     event.preventDefault();
-    // var input = $("input#words").val();
-    // console.log(input);
-    // var result = translate(sentence);
-    var sentence = [];
-    var firstThree = [];
-    $("input#words").each(function(){
-    var wordArray = $(this).val().split(" ");
-
-    // wordArray = (val);
-    sentence.push(wordArray);
-    console.log(sentence);
-    var char = sentence.slice(0,2);
-    firstThree.push(char);
-    console.log(firstThree);
-    // console.log(wordArray);
-    // console.log(char);
-
-  });
-
-  for (var index = 0; index < char.length; index ++) {
-    for(var i = 0; i < 3; i++){
-      var char = sentence.slice(0,2);
-      firstThree.push(char);
-      console.log(firstThree);
-    };
-    // var letterArray = $(this).val().split("",3);
-    // console.log(letterArray);
-  }
-
-    $(".words").text(sentence);
-
-
-
-    // if (!result) {                 // same as writing if (result === false)
-    //   $(".not").text("not");
-    // } else {
-    //   $(".not").text("");
-    // }
-
-    // $("#output").show();
+    var words = $('input#words').val().split(' ');
+    var result = ''
+    for (var i = 0; i <= words.length - 1; i++) {
+      var word = words[i];
+      var letters = word.split('');
+      var index = findVowelIndex(letters);
+      word = word.slice(index) + word.slice(0, index);
+      // if (vowels.includes(letters[0])) {
+      result += ' ' + word
+      if (index === 0 && letters.length > 1) {
+        result += 'way'
+      } else {
+        result += 'ay';
+      }
+        // }
+    }
+    $('#output').text(result).show();
   });
 });
